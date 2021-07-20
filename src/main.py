b576e1b4dad6a21952a9a45f453ff06319c9ece4
RealVortexed <RealVortexed@gmail.com>
2021-07-20 21:08:10 +0100

    Create basic draw_hex function

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 import pygame
+
 pygame.init()
 
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 running = True
+
+
+def draw_hex(vertices):
+    pygame.draw.polygon(screen, (255, 0, 0), vertices, 2)
+
 
 while running:
     screen.fill((255, 255, 255))
