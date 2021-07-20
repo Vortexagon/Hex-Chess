c09de3de7b4055b814a3105929b1ca2dbc952b42
RealVortexed <RealVortexed@gmail.com>
2021-07-20 22:42:55 +0100

    Add hex map and draw using pygame

--- src/main.py
+++ src/main.py
@@ -1,17 +1,23 @@
 import pygame
+
+from hex import HexPixelAdapter, HexMap
+
 pygame.init()
 
 WIDTH, HEIGHT = 800, 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+HEX_MAP = HexMap.from_radius(4)
+ADAPTER = HexPixelAdapter((WIDTH, HEIGHT), (WIDTH / 2, HEIGHT / 2), 30)
 
 
-def draw_hex(vertices):
-    pygame.draw.polygon(screen, (255, 0, 0), vertices, 2)
+def draw_hex(coord):
+    pygame.draw.polygon(SCREEN, (255, 0, 0), ADAPTER.get_vertices(coord), 2)
 
 
 running = True
 while running:
-    screen.fill((255, 255, 255))
+    SCREEN.fill((255, 255, 255))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -21,4 +27,6 @@ while running:
             # code to implement piece movement
             pass
 
+    for cell in HEX_MAP.cells.values():
+        draw_hex(cell.coord)
     pygame.display.update()
