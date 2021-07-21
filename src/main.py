328c5d301652b09d56284edb36e4cf837cb83b1d
RealVortexed <RealVortexed@gmail.com>
2021-07-21 21:59:16 +0100

    Externalise HEX_RADIUS, add image loading, and test demo

--- src/main.py
+++ src/main.py
@@ -7,11 +7,25 @@ pygame.init()
 WIDTH, HEIGHT = 800, 600
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 HEX_MAP = HexMap.from_radius(4)
-ADAPTER = HexPixelAdapter((WIDTH, HEIGHT), (WIDTH / 2, HEIGHT / 2), 30)
+HEX_RADIUS = 30
+ADAPTER = HexPixelAdapter((WIDTH, HEIGHT), (WIDTH / 2, HEIGHT / 2), HEX_RADIUS)
+
+piece_names = [f"{color}_{name}" for color in "wb" for name in ("pawn", "rook", "knight", "bishop", "king", "queen")]
+piece_imgs = {
+    piece_name: pygame.transform.scale(
+        pygame.image.load(f"img/{piece_name}.png").convert_alpha(),
+        (HEX_RADIUS, HEX_RADIUS)
+    )
+    for piece_name in piece_names
+}
 
 
 def draw_hex(coord, fill=False):
     pygame.draw.polygon(SCREEN, (255, 0, 0), ADAPTER.get_vertices(coord), 0 if fill else 2)
+
+    pixel_coords = ADAPTER.hex_to_pixel(coord)
+
+    SCREEN.blit(piece_imgs["w_pawn"], (pixel_coords[0] - HEX_RADIUS / 2, pixel_coords[1] - HEX_RADIUS / 2))
 
 
 hover_coords = (0, 0)
