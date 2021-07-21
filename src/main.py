93fa123b21a5ac5cc7c8c8fb2921cf402854c174
RealVortexed <RealVortexed@gmail.com>
2021-07-21 21:26:19 +0100

    Add hovering hex visualisation

--- src/main.py
+++ src/main.py
@@ -6,27 +6,31 @@ pygame.init()
 
 WIDTH, HEIGHT = 800, 600
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
-
 HEX_MAP = HexMap.from_radius(4)
 ADAPTER = HexPixelAdapter((WIDTH, HEIGHT), (WIDTH / 2, HEIGHT / 2), 30)
 
 
-def draw_hex(coord):
-    pygame.draw.polygon(SCREEN, (255, 0, 0), ADAPTER.get_vertices(coord), 2)
+def draw_hex(coord, fill=False):
+    pygame.draw.polygon(SCREEN, (255, 0, 0), ADAPTER.get_vertices(coord), 0 if fill else 2)
 
 
-running = True
-while running:
-    SCREEN.fill((255, 255, 255))
+hover_coords = (0, 0)
 
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            running = False
+            exit()
         if event.type == pygame.MOUSEBUTTONUP:
             # code to implement piece movement
             pass
+        if event.type == pygame.MOUSEMOTION:
+            hover_coords = pygame.mouse.get_pos()
+
+    SCREEN.fill((255, 255, 255))
 
     for cell in HEX_MAP.cells.values():
         draw_hex(cell.coord)
+    draw_hex(round(ADAPTER.pixel_to_hex(hover_coords)), fill=True)
+
     pygame.display.update()
