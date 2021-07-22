6d95e8840d3e2bdc297b551760f588860da51a96
RealVortexed <RealVortexed@gmail.com>
2021-07-22 21:17:14 +0100

    Add __add__, __sub__, __mul__, __truediv__ and __eq__ to PixelCoord and add unit tests

--- src/pixel.py
+++ src/pixel.py
@@ -2,3 +2,18 @@ class PixelCoord:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def __add__(self, other):
+        return PixelCoord(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return PixelCoord(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return PixelCoord(self.x * other, self.y * other)
+
+    def __truediv__(self, other):
+        return PixelCoord(self.x / other, self.y / other)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
